893d38692903a4bb26781dafeefb2da8fcaace6f
longthp <phamlong.th02@gmail.com>
2023-04-28 15:53:04 +0700

    feat: update install.ps1 with new functions (Make-Shim, Sync-Dotfiles)

--- install.ps1
+++ install.ps1
@@ -1,27 +1,4 @@
-# Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
-# irm get.scoop.sh | iex
-
-function Test-IsAdmin {
-  return (New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
-}
-
-if ((Test-IsAdmin) -eq $false) {
-    Write-Warning "This script requires local admin privileges. Elevating..."
-    gsudo "& '$($MyInvocation.MyCommand.Source)'" $args
-    if ($LastExitCode -eq 999 ) { Write-error 'Failed to elevate.' }
-    return
-}
-
-function Make-Single-Shim {
-    param(
-        [Parameter(Mandatory=$True)][string[]]$Link,
-        [Parameter(Mandatory=$True)][string[]]$Target
-    )
-    gsudo New-Item -ItemType SymbolicLink -Path $Link -Target $Target -Force | Out-Null
-    Write-Host "Made Shim: $Target -> $Link"
-}
-
-$cwd = $(Get-Location)
+$cwd = Get-Location
 $utils = @{
     "$cwd\wt\settings.json" = "$HOME\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"
     "$cwd\pwsh\user_profile.ps1" = "$HOME\Documents\PowerShell\Microsoft.PowerShell_profile.ps1"
@@ -30,13 +7,63 @@ $utils = @{
     "$cwd\cz\.czrc" = "$HOME\.czrc"
     "$cwd\glazewm\config.yaml" = "$HOME\.glaze-wm\config.yaml"
     "$cwd\wsl\.wslconfig" = "$HOME\.wslconfig"
-    "$cwd\lf\lfrc" = "$HOME\AppData\Local\lf\lfrc"
-    "$cwd\lf\colors" = "$HOME\AppData\Local\lf\colors"
-    "$cwd\lf\icons" = "$HOME\AppData\Local\lf\icons"
+    "$cwd\lf" = "$HOME\AppData\Local\lf"
     "$cwd\bottom\bottom.toml" = "$HOME\AppData\Roaming\bottom\bottom.toml"
     "$cwd\bat\config" = "$HOME\AppData\Roaming\bat\config"
     "$cwd\yt-dlp\config" = "$HOME\AppData\Roaming\yt-dlp\config"
     "$cwd\winfetch\config.ps1" = "$HOME\.config\winfetch\config.ps1"
+    "$cwd\fastfetch\config.conf" = "$HOME\.config\fastfetch\config.conf"
 }
 
-foreach ($item in $utils.Keys) { Make-Single-Shim -Link $utils[$item] -Target $item }
+function Make-Shim {
+    param(
+        [Parameter(Mandatory=$True)][string[]]$ShimLink,
+        [Parameter(Mandatory=$True)][string[]]$ShimTarget,
+        [Parameter(Mandatory=$True)][ValidateSet("Leaf", "Container")][string[]]$ShimType
+    )
+    if ("Leaf" -eq $ShimType) {
+        gsudo New-Item -ItemType SymbolicLink -Path $ShimLink -Target $ShimTarget -Force | Out-Null
+        Write-Host "[shim] Created Leaf: $ShimTarget -> $ShimLink"
+    }
+    else {
+        $Leafs = Get-ChildItem -LiteralPath $ShimTarget -Recurse
+        $Leafs.GetEnumerator() | ForEach-Object {
+            $LeafLink = Join-Path -Path $ShimLink -ChildPath $(Split-Path -Path $_ -Leaf)
+            gsudo New-Item -ItemType SymbolicLink -Path $LeafLink -Target $_ -Force | Out-Null
+            Write-Host "[shim] Created CLeaf: $_ -> $LeafLink"
+        }
+        Write-Host "[shim] Created Container: $ShimTarget -> $ShimLink"
+    }
+}
+
+function Sync-Dotfiles {
+    param (
+        [Parameter(Mandatory=$True)][string[]]$Source,
+        [Parameter(Mandatory=$True)][string[]]$Destination
+    )
+    $BackupPath = Join-Path -Path $(Get-Location) -ChildPath ".backup"
+    if (Test-Path -Path $Source -PathType Leaf) {
+        if (Test-Path -Path $Destination -PathType Leaf) {
+            Copy-Item -Path $Destination -Destination $BackupPath -Force
+            # Write-Host "[back] Copied Leaf $Destination -> $BackupPath"
+        }
+        Make-Shim -ShimLink $Destination -ShimTarget $Source -ShimType "Leaf"
+    }
+    elseif (Test-Path -Path $Source -PathType Container) {
+        if (Test-Path -Path $Destination -PathType Container) {
+            Copy-Item -Path $Destination -Destination $BackupPath -Recurse -Force
+            # Write-Host "[back] Copied Container $Destination -> $BackupPath"
+        }
+        Make-Shim -ShimLink $Destination -ShimTarget $Source -ShimType "Container"
+    }
+}
+
+function Install-Dotfiles {
+    gsudo cache on
+    $utils.GetEnumerator() | ForEach-Object {
+        Sync-Dotfiles -Source $_.Key -Destination $_.Value
+    }
+    gsudo cache off
+}
+
+Install-Dotfiles
