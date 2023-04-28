bdd413742a163dcf5bc9ec56672fc04917055edc
longthp <phamlong.th02@gmail.com>
2023-04-28 15:50:29 +0700

    feat: add function to fuzzy select & open with nvim

--- pwsh/user_profile.ps1
+++ pwsh/user_profile.ps1
@@ -3,21 +3,30 @@ Import-Module -Name CompletionPredictor
 Import-Module -Name DockerCompletion
 Import-Module -Name PwshComplete
 Import-Module -Name PSGitCompletions
-# Import-Module -Name PSFzf
 
-Remove-Alias -Name "cat" -Force -ErrorAction Ignore
-Remove-Alias -Name "cp" -Force -ErrorAction Ignore
-Remove-Alias -Name "diff" -Force -ErrorAction Ignore
-Remove-Alias -Name "echo" -Force -ErrorAction Ignore
-Remove-Alias -Name "kill" -Force -ErrorAction Ignore
-Remove-Alias -Name "ls" -Force -ErrorAction Ignore
-Remove-Alias -Name "mv" -Force -ErrorAction Ignore
-Remove-Alias -Name "ps" -Force -ErrorAction Ignore
-Remove-Alias -Name "pwd" -Force -ErrorAction Ignore
-Remove-Alias -Name "mkdir" -Force -ErrorAction Ignore
-Remove-Alias -Name "rm" -Force -ErrorAction Ignore
-Remove-Alias -Name "sleep" -Force -ErrorAction Ignore
-Remove-Alias -Name "tee" -Force -ErrorAction Ignore
+# $Scripts = Join-Path -Path $(Split-Path $PROFILE -Parent) -ChildPath "Script"
+
+function Source-File {
+    param(
+        [Parameter(Mandatory=$True)][string[]]$File
+    )
+    if ( -not (Test-Path -Path $File -PathType Leaf) ) {
+        Write-Host "Skip non-existing script."
+    }
+    else {
+        Write-Host "Loaded: $(Split-Path -Path $File -Leaf)"
+        . $File
+    }
+}
+
+# Source-File -File "$PSScriptRoot\user\env.ps1"
+# Source-File -File "$PSScriptRoot\user\functions.ps1"
+# Source-File -File "$PSScriptRoot\user\options.ps1"
+# Source-File -File "$PSScriptRoot\user\alias.ps1"
+
+$utils = @("cat", "cp", "diff", "echo", "kill", "ls", "mv", "ps", "pwd", "rm", "sleep", "tee")
+ForEach-Object -InputObject $utils -Process {Remove-Alias -Name $_ -Force -ErrorAction Ignore}
+Remove-Item -Path Function:\mkdir -ErrorAction Ignore
 
 $OnViModeChange = [scriptblock]{
     if ($args[0] -eq 'Command') { Write-Host -NoNewLine "`e[4 q" }
@@ -47,64 +56,73 @@ Set-PSReadLineOption -HistoryNoDuplicates
 # Set-PSFzfOption -EnableAliasFuzzyHistory
 
 Set-PSReadLineKeyHandler -Chord Tab -Function MenuComplete
-# Set-PSReadLineKeyHandler -Chord Tab -ScriptBlock { Invoke-FzfTabCompletion }
 Set-PSReadLineKeyHandler -Chord Ctrl+d -Function DeleteChar
+Set-PSReadLineKeyHandler -Chord Ctrl+o -ScriptBlock {
+    [Microsoft.Powershell.PSConsoleReadline]::RevertLine()
+    [Microsoft.Powershell.PSConsoleReadline]::Insert("lfcd.ps1")
+    [Microsoft.Powershell.PSConsoleReadline]::AcceptLine()
+}
 
 if ($env:TERM_PROGRAM -eq "vscode") { Set-PSReadLineKeyHandler -Chord 'Ctrl+w' -Function BackwardKillWord }
 
-# Environment variables
-$env:TERM = "xterm-256color"
-$env:EDITOR = "nvim.exe"
+$Env:TERM = "xterm-256color"
+$Env:EDITOR = "nvim.exe"
 
-$env:STARSHIP_CONFIG = "$HOME\.config\starship\starship.toml"
-$env:STARSHIP_CACHE = "$HOME\.config\starship\Temp"
+$Env:STARSHIP_CONFIG = "$HOME\.config\starship\starship.toml"
+$Env:STARSHIP_CACHE = "$HOME\.config\starship\Temp"
 
-$env:FZF_DEFAULT_OPTS=@"
+$Env:FZF_DEFAULT_OPTS=@"
 --height=40% --layout=reverse --info=inline --no-scrollbar --no-separator
 --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8
 --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc
 --color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8
 "@
-$env:_ZO_FZF_OPTS = $FZF_DEFAULT_OPTS
+$Env:_ZO_FZF_OPTS = $Env:FZF_DEFAULT_OPTS
 
-$env:RUST_BACKTRACE = 1
+$Env:RUST_BACKTRACE = 1
 
-$env:PYENV_ROOT = "$HOME\.pyenv\pyenv-win"
-$env:PYENV_HOME = "$HOME\.pyenv\pyenv-win"
-$env:PYENV = "$HOME\.pyenv\pyenv-win"
+$Env:PYENV_ROOT = "$HOME\.pyenv\pyenv-win"
+$Env:PYENV_HOME = "$HOME\.pyenv\pyenv-win"
+$Env:PYENV = "$HOME\.pyenv\pyenv-win"
 
-$env:PATH += ";$HOME\scoop\apps\git\current\usr\bin"
-$env:PATH += ";C:\Softwares\node"
-$env:PATH += ";C:\Softwares\Python\Python310\"
-$env:PATH += ";C:\Softwares\Python\Python310\Scripts"
+$Env:PATH += ";$HOME\scoop\apps\git\current\usr\bin"
+$Env:PATH += ";$HOME\.pyenv\pyenv-win\bin"
+$Env:PATH += ";$HOME\.pyenv\pyenv-win\shims"
+$Env:PATH += ";C:\Softwares\node"
+$Env:PATH += ";C:\Softwares\Python\Python310"
+$Env:PATH += ";C:\Softwares\Python\Python310\Scripts"
 
+function so { . $PROFILE }
 function which ($command) { Get-Command -Name $command -ErrorAction SilentlyContinue | Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue }
 
 function ls { ls.exe '--color=always' '--group-directories-first' '--ignore={"NTUSER.DAT*","ntuser.dat*"}' $args[0] }
 function ll { ls.exe '--color=always' '--group-directories-first' '--show-control-chars' '-CFGNlhap' '--ignore={"NTUSER.DAT*","ntuser.dat*"}' $args[0] }
 
 function wm { Start-Process glazewm -ArgumentList "--config $HOME\.glaze-wm\config.yaml" -WindowStyle Hidden }
-function bb { Start-Process btm -ArgumentList "--basic"}
 
-# Aliases
+
 function Activate-Venv {
-  $venv_home = "C:\Softwares\Python\venvs"
-  $venv_name = (Get-ChildItem $venv_home -Directory).Name | Invoke-Fzf
-  $venv_path = "$venv_home\$venv_name\Scripts\Activate.ps1"
+    $VENV_HOME = "C:\Softwares\Python\venvs"
+    $VENV_NAME = Get-ChildItem $VENV_HOME -Directory -Name | fzf
+    $VENV_SCRIPT = "$VENV_HOME\$VENV_NAME\Scripts\Activate.ps1"
 
-  if ( -not (Test-Path -Path $venv_path) ) { Write-Host "None selected" }
-  else { Invoke-Expression $venv_path }
+    if ( (Test-Path -Path $VENV_SCRIPT -PathType Leaf) -And !($null -eq $VENV_NAME) ) { Invoke-Expression $VENV_SCRIPT }
 }
 
-Set-Alias "so" ". $PROFILE"
-Set-Alias "co" "codium"
+function Nvim-Fzf {
+    $SelectedFile = Get-ChildItem -Path $(Get-Location) -Recurse -Force -File -Name | fzf
+    if ( -not ($null -eq $SelectedFile) ) { Invoke-Expression "nvim $SelectedFile" }
+}
+
 Set-Alias "lzg" "lazygit"
 Set-Alias "lzd" "lazydocker"
 Set-Alias "dl" "yt-dlp"
-Set-Alias "se" "$HOME\scoop\apps\sioyek\2.0.0\sioyek.exe"
+Set-Alias "da" "deactivate"
+Set-Alias "co" "codium"
 Set-Alias "av" Activate-Venv
+Set-Alias "nf" Nvim-Fzf
+Set-Alias "ff" "fastfetch"
 
-# Init
 Invoke-Expression (& {
     $hook = if ($PSVersionTable.PSVersion.Major -lt 6) { 'prompt' } else { 'pwd' }
     (zoxide init --hook $hook powershell | Out-String)
