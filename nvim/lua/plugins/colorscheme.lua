0b93e27dc597a0e8a393449a682a1db3d8738f81
longthp <phamlong.th02@gmail.com>
2023-06-06 08:32:04 +0700

    feat: update to Catppuccin Mocha OLED

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -5,6 +5,9 @@ return {
     config = function()
         require("catppuccin").setup({
             flavour = "mocha",
+            background = {
+                dark = "mocha",
+            },
             term_colors = true,
             styles = {
                 comments = { "italic" },
@@ -12,7 +15,27 @@ return {
             no_italic = true,
             transparent_background = false,
             show_end_of_buffer = false,
+            color_overrides = {
+                mocha = {
+                    base = "#000000",
+                    mantle = "#010101",
+                    crust = "#020202",
+                }
+            },
+            integrations = {
+                treesitter = true,
+                cmp = true,
+                gitsigns = true,
+                bufferline = true,
+                indent_blankline = {
+                    enabled = true,
+                    colored_indent_levels = false,
+                },
+                mason = true,
+                nvimtree = true,
+                telescope = true,
+            },
         })
-        vim.cmd.colorscheme "catppuccin"
+        vim.cmd [[colorscheme catppuccin]]
     end,
 }
