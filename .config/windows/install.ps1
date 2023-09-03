6806a4871afb4e40a3bea7ce50acabceca6ea696
longthp <phamlong.th02@gmail.com>
2023-09-03 21:48:37 +0700

    feat(windows-install): refactor install script; add loop to remove old symlinks

--- .config/windows/install.ps1
+++ .config/windows/install.ps1
@@ -1,53 +1,60 @@
-$Mappings = [ordered]@{
-    ".config/git" = "$HOME"
-    ".config/wsl" = "$HOME"
-    ".config/windows/glazewm" = "$HOME\.glaze-wm"
-    ".config/windows/pwsh" = "$HOME\Documents\PowerShell"
-    ".config/windows/wt" = "$HOME\scoop\persist\windows-terminal\settings"
-    ".config/starship" = "$HOME\.config\starship"
-    ".config/fastfetch" = "$HOME\.config\fastfetch"
-    ".config/wezterm" = "$HOME\.config\wezterm"
-    ".config/bottom" = "$Env:APPDATA\bottom"
-    ".config/bat" = "$Env:APPDATA\bat"
-    ".config/yt-dlp" = "$Env:APPDATA\yt-dlp"
-    ".config/lf" = "$Env:LOCALAPPDATA\lf"
-    ".config/nvim" = "$Env:LOCALAPPDATA\nvim"
-}
-
-
 function Test-IsAdministrator {
-    return ([Security.Principal.WindowsPrincipal]`
-            [Security.Principal.WindowsIdentity]::GetCurrent()`
+    return (
+        [Security.Principal.WindowsPrincipal]`
+        [Security.Principal.WindowsIdentity]::GetCurrent()`
     ).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator) -and $env:USERNAME -ne 'WDAGUtilityAccount'
 }
 
-if ( Test-IsAdministrator -eq $False ) {
+function Test-CommandAvailable {
+    param (
+        [Parameter(Mandatory=$True, Position=0)][String]$Command
+    )
+    return [Boolean](Get-Command $Command -ErrorAction SilentlyContinue)
+}
+
+function Install-Dotfiles {
+    param (
+        [System.Array]$Applications
+    )
+
+    $Applications | ForEach-Object -Process {
+        if ($_.source) {
+            $S_Parent = Resolve-Path -Path $_.source
+            $D_Parent = New-Item -ItemType Directory -Path $_.dest -Force | Resolve-Path
+
+            if ($_.dest -ne "~") {
+                # remove old symbolic links
+                Get-ChildItem -Recurse $D_Parent -File | ForEach-Object -Process {
+                    if (!(Test-Path $_.ResolvedTarget)) {
+                        Write-Host "[clean] removing [$_]"
+                        Remove-Item -Path $_
+                    }
+                }
+            }
+
+            # add actual symbolic links
+            Get-ChildItem -Recurse $_.source -File | ForEach-Object -Process {
+                $S_Item_FullName = $_.FullName.Split($S_Parent).Get(1)
+                $D_Symlink = Join-Path -Path $D_Parent -ChildPath $S_Item_FullName
+
+                Write-Host "[shim] $_ => $($D_Symlink.Split('::').Get(1))"
+                New-Item -ItemType SymbolicLink -Path $D_Symlink -Target $_ -Force | Out-Null
+            }
+        }
+    }
+}
+
+
+# start installation
+$apps = Import-Csv -Path "$Env:USERPROFILE\dotfiles\tmp\apps.csv"
+
+if ((Test-IsAdministrator) -eq $false) {
     Write-Warning "This script requires local admin privileges. Elevating..."
-    gsudo "& '$( $MyInvocation.MyCommand.Source )'" $args
-    if ( $LastExitCode -eq 999 ) {
-        Write-Error 'Failed to elevate.'
+    sudo "& '$($MyInvocation.MyCommand.Source)'" $args
+    if ($LastExitCode -eq 999 ) {
+        Write-error 'Failed to elevate.'
     }
     return
 }
 
-function Install-Dotfiles {
-    $Mappings.GetEnumerator().ForEach({
-        $S_Parent = Resolve-Path -Path $_.Key
-        $D_Parent = $_.Value
-        if (!(Test-Path $D_Parent)) {
-            Write-Host "[warn] Creating missing folder:" $D_Parent
-            New-Item -ItemType Directory $D_Parent | Out-Null
-        }
-
-        $S_Items = Get-ChildItem -Recurse $_.Key -File
-        $S_Items.ForEach({
-            $S_Item_FullName = $_.FullName.Split($S_Parent).Get(1)
-            $D_Symlink = Join-Path -Path $D_Parent -ChildPath $S_Item_FullName
-
-            Write-Host "[info] $_ => $D_Symlink"
-            New-Item -ItemType SymbolicLink -Path $D_Symlink -Target $_ -Force | Out-Null
-        })
-    })
-}
-
-Install-Dotfiles
+Install-Dotfiles -Applications $apps
