8e663417675d7df0f1ac141006e4c3a1ec643e2d
longthp <phamlong.th02@gmail.com>
2023-12-04 14:30:27 +0700

    feat(wezterm): update local config var; create get_default_prog function

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,8 +1,6 @@
 local wezterm = require("wezterm")
-local act = require("wezterm").action
-local c = wezterm.config_builder()
+local config = wezterm.config_builder()
 
-require("theme").apply(c)
 
 local function get_os()
     local target = wezterm.target_triple
@@ -15,74 +13,107 @@ local function get_os()
     end
 end
 
+local function get_default_prog()
+    if get_os() == "windows" then
+        DEFAULT_PROG = {
+            "C:/Program Files/PowerShell/7-preview/pwsh.exe",
+            "-NoLogo",
+            "-ExecutionPolicy", "ByPass"
+        }
 
-if get_os() == "windows" then
-    default_prog = {
-        "C:/Program Files/PowerShell/7-preview/pwsh.exe",
-        "-NoLogo",
-        "-ExecutionPolicy", "ByPass"
-    }
-    -- default_prog = {
-    --     "nu.exe", "--execute", "clear"
-    -- }
-
--- elseif get_os() == "linux" then
---     default_prog = {}
+    elseif get_os() == "linux" then
+        DEFAULT_PROG = {
+            "/usr/bin/bash",
+            "-l"
+        }
+    end
+    return DEFAULT_PROG
 end
 
-wezterm.on('format-tab-title', function (tab, _, _, _, _)
-    return {
-        { Text = ' ' .. tab.tab_index + 1 .. ' ' },
-    }
-end)
+
+wezterm.on(
+    'format-tab-title',
+    function (tab, tabs, panes, config, hover, max_width)
+        return {
+            { Text = ' ' .. tab.tab_index + 1 .. ' ' },
+        }
+    end
+)
 
 
-c.default_prog = default_prog
-c.font = wezterm.font_with_fallback({
+config.default_prog = get_default_prog()
+
+config.color_schemes = require("theme")
+config.color_scheme = "Custom"
+
+config.font = wezterm.font_with_fallback({
     "JetBrainsMono Nerd Font",
     { family = "Symbols Nerd Font Mono", scale = 1.0 },
 })
 
-c.use_cap_height_to_scale_fallback_fonts = true
-c.font_size = 12
-c.line_height = 1.0
-c.pane_focus_follows_mouse = false
-c.warn_about_missing_glyphs = false
-c.show_update_window = false
-c.check_for_updates = false
+config.use_cap_height_to_scale_fallback_fonts = true
+config.font_size = 11
+config.line_height = 1.0
+config.pane_focus_follows_mouse = false
+config.warn_about_missing_glyphs = false
+config.show_update_window = false
+config.check_for_updates = false
 
-c.window_decorations = "RESIZE"
-c.window_close_confirmation = "NeverPrompt"
-c.window_background_opacity = 1.0
-c.window_padding = {
+config.window_decorations = "RESIZE"
+config.window_close_confirmation = "NeverPrompt"
+config.window_background_opacity = 1.0
+config.window_padding = {
     left = 12,
     right = 12,
     top = 12,
-    bottom = 0,
+    bottom = 3,
 }
-c.adjust_window_size_when_changing_font_size = false
 
-c.inactive_pane_hsb = {
+config.adjust_window_size_when_changing_font_size = false
+
+config.inactive_pane_hsb = {
     saturation = 1.0,
     brightness = wezterm.GLOBAL.is_dark and 0.90 or 0.95,
 }
-c.enable_scroll_bar = false
+config.enable_scroll_bar = false
 
-c.tab_max_width = 50
-c.tab_bar_at_bottom = true
-c.use_fancy_tab_bar = false
-c.show_new_tab_button_in_tab_bar = false
-c.hide_tab_bar_if_only_one_tab = true
+config.tab_max_width = 50
+config.tab_bar_at_bottom = true
+config.use_fancy_tab_bar = false
+config.show_new_tab_button_in_tab_bar = false
+config.hide_tab_bar_if_only_one_tab = true
 
-c.disable_default_key_bindings = false
-c.force_reverse_video_cursor = true
-c.launch_menu = {
-  { label = "Process Viewer", args = { "btm", "--basic" } },
+config.disable_default_key_bindings = false
+config.force_reverse_video_cursor = true
+config.launch_menu = {
+    { label = "Process Viewer", args = { "btm", "--basic" } },
+    {
+        label = "PowerShell Core",
+        args = {
+            "C:/Program Files/PowerShell/7-preview/pwsh.exe",
+            "-NoLogo",
+            "-ExecutionPolicy", "ByPass"
+        },
+    },
+    {
+        label = "NuShell",
+        args = {
+            "C:/Users/long/scoop/apps/nu/current/nu.exe",
+            "-l"
+        },
+    },
+    {
+        label = "Zsh",
+        args = {
+            "C:/Softwares/PortableGit/usr/bin/zsh.exe",
+            "-l"
+        }
+    }
 }
 
-c.keys = require("keys")
+config.keys = require("keys")
 
-c.hyperlink_rules = {
+config.hyperlink_rules = {
     {
         regex = "\\b\\w+://[\\w.-]+:[0-9]{2,15}\\S*\\b",
         format = "$0",
@@ -109,4 +140,4 @@ c.hyperlink_rules = {
     },
 }
 
-return c
+return config
