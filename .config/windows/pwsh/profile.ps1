73b9a5edfd494165794d6a9a54de8a25c700f5c8
longthp <phamlong.th02@gmail.com>
2023-12-04 14:32:47 +0700

    feat(pwsh): reorganize profile.ps1 with vim folding

--- .config/windows/pwsh/profile.ps1
+++ .config/windows/pwsh/profile.ps1
@@ -1,31 +1,30 @@
-$utils = @("cat", "cp", "diff", "echo", "kill", "ls", "mv", "ps", "pwd", "rm", "sleep", "tee")
-$utils.ForEach({ Remove-Alias -Name $_ -Force -ErrorAction Ignore })
-Remove-Item -Path Function:\mkdir -ErrorAction Ignore
+# vim:fileencoding=utf-8:foldmethod=marker
 
-$OnViModeChange = [ScriptBlock]{
-    if ($args[0] -eq 'Command') { Write-Host -NoNewLine "`e[4 q" }
-    else { Write-Host -NoNewLine "`e[0 q" }
-}
-
+# PSReadline {{{
 Set-PsReadLineOption -EditMode Vi
-Set-PSReadLineOption -ViModeIndicator Script -ViModeChangeHandler $OnViModeChange
 Set-PSReadLineOption -BellStyle None
 Set-PSReadlineOption -Color @{
     "Command" = [ConsoleColor]::White
-    "Parameter" = [ConsoleColor]::DarkGray
-    "Operator" = [ConsoleColor]::DarkGray
+    "Parameter" = [ConsoleColor]::Gray
+    "Operator" = [ConsoleColor]::Gray
     "Variable" = [ConsoleColor]::White
     "String" = [ConsoleColor]::Gray
     "Number" = [ConsoleColor]::Gray
     "Type" = [ConsoleColor]::Gray
-    "Comment" = [ConsoleColor]::DarkGray
-    "InlinePrediction" = [ConsoleColor]::DarkGray
+    "Comment" = [ConsoleColor]::Gray
+    "InlinePrediction" = [ConsoleColor]::Gray
     "Keyword" = [ConsoleColor]::White
 }
 
 Set-PSReadLineOption -PredictionSource History
 Set-PSReadLineOption -PredictionViewStyle InlineView
 Set-PSReadLineOption -HistoryNoDuplicates
+
+# $OnViModeChange = [ScriptBlock]{
+#     if ($args[0] -eq 'Command') { Write-Host -NoNewLine "`e[4 q" }
+#     else { Write-Host -NoNewLine "`e[0 q" }
+# }
+# Set-PSReadLineOption -ViModeIndicator Script -ViModeChangeHandler $OnViModeChange
 
 Set-PSReadLineKeyHandler -Chord Tab -Function MenuComplete
 Set-PSReadLineKeyHandler -Chord Ctrl+d -Function DeleteChar
@@ -36,7 +35,9 @@ Set-PSReadLineKeyHandler -Chord Ctrl+o -ScriptBlock {
 }
 
 if ($Env:TERM_PROGRAM -eq "vscode") { Set-PSReadLineKeyHandler -Chord 'Ctrl+w' -Function BackwardKillWord }
+# }}}
 
+# Environment Variables {{{
 $Env:TERM = "xterm-256color"
 $Env:EDITOR = "nvim.exe"
 $Env:GIT_SSH = "C:\WINDOWS\System32\OpenSSH\ssh.exe"
@@ -52,22 +53,35 @@ $Env:FZF_DEFAULT_OPTS = @"
 "@
 $Env:_ZO_FZF_OPTS = $Env:FZF_DEFAULT_OPTS
 
-$Env:RUST_BACKTRACE = 1
-$Env:RUSTFLAGS = "-C link-arg=-lmsvcrt"
+# $Env:RUST_BACKTRACE = 1
+# $Env:RUSTFLAGS = "-C link-arg=-lmsvcrt"
 $Env:YAZI_CONFIG_HOME = "$HOME\.config\yazi"
+$Env:RCLONE_CONFIG = "$Env:USERPROFILE\.config\rclone\rclone.conf"
 
-$Env:PATH += ";C:\Softwares\node"
 $Env:PATH += ";$HOME\scoop\apps\git\current\usr\bin"
 $Env:PATH += ";$HOME\.local\bin"
+# }}}
 
+# Functions {{{
 function so { . $PROFILE.CurrentUserAllHosts }
-function av { . ".\venv\Scripts\Activate.ps1" }
+function av { . "$PWD\*venv\Scripts\Activate.ps1" }
 function which ($command) { Get-Command -Name $command -ErrorAction SilentlyContinue | Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue }
 
+if (Get-Command -Name "exa" -ErrorAction SilentlyContinue) {
+    $ls_exe = "exa.exe"
+    $ls_params = @('--color=always', '--group-directories-first', '--ignore-glob "NTUSER.DAT*|ntuser.dat*"')
+    $ll_params = @('-la', '--icons')
+}
+else {
+    $ls_exe = "ls.exe"
+    $ls_params = @('--color=always', '--group-directories-first', '--ignore={"NTUSER.DAT*", "ntuser.dat*"}')
+    $ll_params = @('-la')
+}
+function ls { Invoke-Expression -Command "$ls_exe $ls_params $Args" }
+function ll { Invoke-Expression -Command "$ls_exe $ls_params $ll_params $Args" }
 
-$ls_params = @('--color=always', '--group-directories-first', '--ignore={"NTUSER.DAT*", "ntuser.dat*"}')
-function ls ($item) { Invoke-Command -ScriptBlock { ls.exe $ls_params $item } }
-function ll ($item) { Invoke-Command -ScriptBlock { ls.exe '--show-control-chars' '-CFGNlhap' $ls_params $item } }
+function vim { Invoke-Expression -Command "nvim.exe -u '$Env:USERPROFILE/dotfiles/init.vim' $Args" }
+function pyv { Invoke-Expression -Command "python.exe '$Env:USERPROFILE/.local/bin/virtualenv.pyz' $Args" }
 
 function wm {
     if (!(Get-Process -Name GlazeWM -ErrorAction SilentlyContinue)) {
@@ -122,23 +136,50 @@ function Ripgrep-Fzf {
 
 
 function Start-SSH-Agent {
-    begin {
-        $Status = $(Get-Service -Name "ssh-agent").Status
-    }
-    process {
-        if ($Status -eq "Stopped") {
-            Start-Service -Name "ssh-agent"
+    $Status = $(Get-Service -Name "ssh-agent").Status
+    if ($Status -eq "Stopped") {
+        Start-Service -Name "ssh-agent"
 
-            $SelectedKey = Invoke-Command -ScriptBlock { Get-ChildItem -Path ~/.ssh/ -Exclude *.pub,config,known_host* -Name | fzf.exe }
-            Invoke-Command -ScriptBlock { ssh-add $HOME/.ssh/$SelectedKey  }
-            Write-Host "SSH Agent started"
-        }
-        else {
-            Write-Host "SSH Agent already running"
-        }
+        $SelectedKey = Invoke-Command -ScriptBlock { Get-ChildItem -Path ~/.ssh/ -Exclude *.pub,config,known_host* -Name | fzf.exe }
+        Invoke-Command -ScriptBlock { ssh-add $HOME/.ssh/$SelectedKey  }
+        Write-Host "SSH Agent started"
+    }
+    else {
+        Write-Host "SSH Agent already running"
     }
 }
 
+function br {
+    $args = $args -join ' '
+    $cmd_file = New-TemporaryFile
+    $process = Start-Process -FilePath 'broot.exe' `
+                             -ArgumentList "--outcmd $($cmd_file.FullName) $args" `
+                             -NoNewWindow -PassThru -WorkingDirectory $PWD
+
+    Wait-Process -InputObject $process
+    if ($process.ExitCode -eq 0) {
+        $cmd = Get-Content $cmd_file
+        Remove-Item $cmd_file
+        if ($cmd -ne $null) { Invoke-Expression -Command $cmd }
+    } else {
+        Remove-Item $cmd_file
+        Write-Host "`n"
+        Write-Error "broot.exe exited with error code $($process.ExitCode)"
+    }
+}
+
+function Invoke-Starship-PreCommand {
+    $loc = $executionContext.SessionState.Path.CurrentLocation;
+    $prompt = "$([char]27)]9;12$([char]7)"
+    if ($loc.Provider.Name -eq "FileSystem")
+    {
+        $prompt += "$([char]27)]9;9;`"$($loc.ProviderPath)`"$([char]27)\"
+    }
+    $host.ui.Write($prompt)
+}
+# }}}
+
+# Aliases {{{
 Set-Alias "lzg" "lazygit"
 Set-Alias "lzd" "lazydocker"
 Set-Alias "dl" "yt-dlp"
@@ -150,16 +191,15 @@ Set-Alias "nf" Nvim-Fzf
 Set-Alias "rf" Ripgrep-Fzf
 Set-Alias "mf" Mpv-Fzf
 Set-Alias "sa" Start-SSH-Agent
+# }}}
 
-function Invoke-Starship-PreCommand {
-    $loc = $executionContext.SessionState.Path.CurrentLocation;
-    $prompt = "$([char]27)]9;12$([char]7)"
-    if ($loc.Provider.Name -eq "FileSystem")
-    {
-        $prompt += "$([char]27)]9;9;`"$($loc.ProviderPath)`"$([char]27)\"
-    }
-    $host.ui.Write($prompt)
-}
+# {{{
+$utils = @("cat", "cp", "diff", "echo", "kill", "ls", "mv", "ps", "pwd", "rm", "sleep", "tee")
+$utils.ForEach({ Remove-Alias -Name $_ -Force -ErrorAction Ignore })
+Remove-Item -Path Function:\mkdir -ErrorAction Ignore
+# }}}
 
+# Init {{{
 Invoke-Expression ( & { (starship init powershell | Out-String) } )
 Invoke-Expression ( & { (zoxide init powershell | Out-String) } )
+# }}}
